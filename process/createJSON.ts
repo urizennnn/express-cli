e0f507246f72cde77e2f9a946d9e45852b80109c
urizennnn <igamerryt@gmail.com>
2024-04-08 16:04:02 +0100

    updated code

--- process/createJSON.ts
+++ process/createJSON.ts
@@ -1,16 +1,49 @@
 import chalk from "chalk";
-import {exec} from "child_process";
-import path from "path"
-export function createJsonUponFreshStart (PackageManager:string,name:string) {
-	if (!PackageManager) {
-		console.log(chalk.red('Error retrieving PackageManager'))
-		process.exit(1)
-	}
-	process.chdir(name)
-	exec(`${PackageManager} init -y`,(error:Error | null)=>{
-		if (error) {
-			console.log(`I could not generate package.json.\n Please try running "${PackageManager} init -y" yourself.`)
-		}
-	})
-}
+import { exec } from "child_process";
 
+export function createJsonUponFreshStart(PackageManager: string, name: string) {
+  if (!PackageManager) {
+    console.log(chalk.red("Error retrieving PackageManager"));
+    process.exit(1);
+  }
+
+  process.chdir(name);
+
+  const generatePackageJson = new Promise<void>((resolve, reject) => {
+    exec(`${PackageManager} init -y`, (error: Error | null) => {
+      if (error) {
+        console.error(`Error generating package.json: ${error}`);
+        reject(error);
+      } else {
+        console.log(
+          `Successfully generated package.json using ${PackageManager}`
+        );
+        resolve();
+      }
+    });
+  });
+
+  const installExpress = new Promise<void>((resolve, reject) => {
+    exec(`${PackageManager} install express`, (error: Error | null) => {
+      if (error) {
+        console.error(chalk.red(`Error installing express: ${error}`));
+        reject(error);
+      } else {
+        console.log(chalk.red(`Successfully installed express using ${PackageManager}`));
+        resolve();
+      }
+    });
+  });
+
+
+  generatePackageJson
+    .then(() => {
+      return installExpress;
+    })
+    .catch((error) => {
+      console.error(
+        chalk.red(`Error during package.json generation or express installation: ${error}`)
+      );
+      process.exit(1);
+    });
+}
